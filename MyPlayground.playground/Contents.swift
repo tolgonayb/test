32eae6767ae3d16f0d419508ac906e9f223b4ac5
tolgonayb <tolgonayb@gmail.com>
2021-02-25 19:10:29 +0600

    my playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,55 @@
 import UIKit
 
-var str = "Hello, playground"
+import Darwin
+
+let a: Int = 4
+let b: Int = 4
+
+let c: Int = Int(pow(Double(a), Double(b)))
+
+
+let string = "Bonjour 👋🏻"
+let characters = Array(string)
+print(characters)
+
+
+
+func i(y:String,x:String) -> Bool {
+var charY = Array(y)
+var charX = Array(x)
+if let x = charX.firstIndex(of: "#") { charX[x] = " "
+}
+else if let xx = charX.lastIndex(of: "#") { charX[xx] = " "
+}
+
+if let y = charY.firstIndex(of: "#") { charY[y] = " "
+}
+else if let yy = charY.lastIndex(of: "#") { charY[yy] = " "
+}
+
+for char in charY.reversed() {
+
+for char2 in charX.reversed(){
+    if char=="#"&&char2=="#"&&char==char2{
+     
+  print(char)
+print(char2)
+        return true }
+    else if char==char2{
+   print(char)
+print(char2)
+ return true
+ }
+return false
+ }}
+ return false}
+
+if i(y:"t",x:"t")==true{
+ 
+ 
+  print ("Правильно")
+    
+}
+else {
+    print("Неправильно")
+}
